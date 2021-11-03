43aba8abd784264415d81ca0cd8945dfecdf0d39
hmachefe <hugo.machefer@gmail.com>
2021-11-03 00:14:00 -0400

    add config files in environment folder

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  pokemonApiUrl: {
+    collection: 'https://pokeapi.co/api/v2/pokemon',
+    speciesUrl: 'https://pokeapi.co/api/v2/pokemon-species'
+  },
+  maxItemsByPage: 12
+};
